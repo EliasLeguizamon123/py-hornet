4c06de5d902dec7e43546c93b1ee6992dabb6096
Elias Leguizamon <elias.leguizamon1997@gmail.com>
2023-01-27 15:23:56 -0300

    fix pront in screen, try while loop for pagination

--- book.py
+++ book.py
@@ -1,5 +1,7 @@
+import re
 import ebooklib
 from bs4 import BeautifulSoup
+import os
 
 def readEpub(book): 
     chapters = []
@@ -10,12 +12,14 @@ def readEpub(book):
     
     for chapter in chapters :
         soup = BeautifulSoup(chapter, 'html.parser')
-        text = soup.find_all(text = True)
-        chapterText = []
-        for t in text :
-            if t.parent.name not in ['[document]', 'noscript', 'header', 'html', 'meta', 'head', 'input', 'script'] :
-                chapterText.append(str(t))
-        output.append(''.join(chapterText))
+        # text = soup.find_all(text = True)
+        text = soup.get_text()
+        output.append(text)
+    #     chapterText = []
+    #     for t in text :
+    #         if t.parent.name not in ['[document]', 'noscript', 'header', 'html', 'meta', 'head', 'input', 'script'] :
+    #             chapterText.append(str(t))
+    #     output.append(''.join(chapterText))
     return output
     
 def getContent(items):
@@ -24,3 +28,26 @@ def getContent(items):
         if item.get_type() == ebooklib.ITEM_DOCUMENT:
             chapters.append(item.get_content())
     return chapters
+
+def createPages(output, size):
+    columns = size[1]
+    rows = size[0]
+    pages = []
+    for chapter in output:
+        page = []
+        chapter = re.sub(r'\n(?=\n)', '', chapter)
+        for y in range(rows - 3):
+            if y * columns >= len(chapter):
+                break
+            line = ''
+            for x in range(columns):
+                index = y * columns + x
+                
+                if index >= len(chapter):
+                    break
+                line += chapter[index]
+                
+            page.append(line)
+        pages.append('\n'.join(page))
+    
+    return pages
--- main.py
+++ main.py
@@ -1,46 +1,51 @@
-import curses
-import curses.ascii
-from args import generateArgs
-from book import readEpub, getContent
-import os
+import curses, os, curses.ascii
 from time import sleep
+from book import createPages
+from args import generateArgs
     
 def mainScreen(screen):
     output = generateArgs()
-    columns = os.get_terminal_size().columns
-    rows = os.get_terminal_size().lines
-    pages = []
+    size = screen.getmaxyx();
+    rows = size[0]
+    columns = size[1]
+    pages = createPages(output, size)
+    x = 1
     y = 1
-    x = 0
-    for chapters in output:
-        for character in chapters:
-            if y < rows - 3:
-                if x < columns:
-                    screen.addstr(y, x, character)
-                    sleep(0.0001)
-                    screen.refresh()
-                    x += 1
-                else :
-                    y += 1
-                    x = 0
-            else:
-                key = screen.getch()
-                screen.clear()
-                y = 1
-                continue         
+    index = 0
+    page = pages[index]
+    
+    while True: 
         
-    # while 1:
-    # key = screen.getch()
-    #     screen.clear()
-    #     if key == curses.KEY_UP:
-    #         screen.addstr(0, 0, "You pressed Up key!")
-    #     elif key == curses.KEY_DOWN:
-    #         screen.addstr(0, 0, "You pressed Down key!")
-    #     elif key == curses.KEY_ENTER or key in [10, 13]:
-    #         screen.addstr(0, 0, "You pressed Enter.")
-    #     elif key == curses.ascii.ESC or ord('q'):
-    #         return
+        for word in page.split():
+            if y <= rows - 3:
+                if x + len(word) > columns - 1:
+                    y += 1
+                    x = 1
+
+                screen.addstr(y, x, word)
+                screen.addstr(y, x - 1, ' ')
+                sleep(0.0001)
+                screen.refresh()
+                x += len(word) + 1
+            else :
+                y = 1
+                x = 1
+                screen.clear()
+                screen.addstr(y, x, word)
+                screen.addstr(y, x - 1, ' ')
+                sleep(0.0001)
+                screen.refresh()
+                x += len(word) + 1
+        
+        key = screen.getch()
+        
+        if key == curses.KEY_UP:
+            index += 1
+        if key == curses.KEY_DOWN and index >= 1:
+            index -= 1
+            
 
 
 if __name__ == "__main__":
-  curses.wrapper(mainScreen)
+    curses.wrapper(mainScreen)
+    # mainScreen()
