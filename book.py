f5a9dde0804053d2c6dd786a7f878e244241480f
Elias Leguizamon <elias.leguizamon1997@gmail.com>
2023-01-31 14:33:26 -0300

    fix page divider in books

--- book.py
+++ book.py
@@ -1,25 +1,16 @@
-import re
 import ebooklib
 from bs4 import BeautifulSoup
-import os
+import textwrap
 
 def readEpub(book): 
     chapters = []
     items = list(book.get_items())
     output = []
-    
     chapters = getContent(items)
-    
     for chapter in chapters :
         soup = BeautifulSoup(chapter, 'html.parser')
-        # text = soup.find_all(text = True)
         text = soup.get_text()
         output.append(text)
-    #     chapterText = []
-    #     for t in text :
-    #         if t.parent.name not in ['[document]', 'noscript', 'header', 'html', 'meta', 'head', 'input', 'script'] :
-    #             chapterText.append(str(t))
-    #     output.append(''.join(chapterText))
     return output
     
 def getContent(items):
@@ -34,20 +25,7 @@ def createPages(output, size):
     rows = size[0]
     pages = []
     for chapter in output:
-        page = []
-        chapter = re.sub(r'\n(?=\n)', '', chapter)
-        for y in range(rows - 3):
-            if y * columns >= len(chapter):
-                break
-            line = ''
-            for x in range(columns):
-                index = y * columns + x
-                
-                if index >= len(chapter):
-                    break
-                line += chapter[index]
-                
-            page.append(line)
-        pages.append('\n'.join(page))
-    
+        lines = textwrap.wrap(chapter, width=columns)
+        for i in range(0, len(lines), rows):
+            pages.append('\n'.join(lines[i:min(i + rows, len(lines))]))
     return pages
