d6c2099de934ecb43250e4267b990dc90f38d901
Elias Leguizamon <elias.leguizamon1997@gmail.com>
2023-02-22 17:25:36 -0300

    add try catch in reading epub file

--- args.py
+++ args.py
@@ -5,13 +5,18 @@ from book import readEpub
 def generateArgs():
     # Create Arguments
     parser = argparse.ArgumentParser('Hornet is a cli/terminal epub reader made in python')
-    parser.add_argument("-b", "--book",  help="Select one book to read")
+    parser.add_argument("-b", "--book", help="Select one book to read", required=False, nargs='+', type=str)
     args = parser.parse_args()
+    print(args)
     if args.book is not None:
-        book = epub.read_epub(args.book)
-        bookTitle = book.get_metadata('DC', 'title')
-        bookPath = os.path.abspath(str(sys.argv[2]))
-        return {"content": readEpub(book), "bookPath": bookPath, "bookTitle": bookTitle[0][0] }
+        try :
+            book = epub.read_epub(args.book)
+            bookTitle = book.get_metadata('DC', 'title')
+            bookPath = os.path.abspath(str(sys.argv[2]))
+            return {"content": readEpub(book), "bookPath": bookPath, "bookTitle": bookTitle[0][0] }
+        except :
+            print('This is not a epub file')
+            sys.exit(1)
     else:
         parser.print_help()
         sys.exit(1)
